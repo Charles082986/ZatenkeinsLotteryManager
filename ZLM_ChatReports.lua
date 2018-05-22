63f76f1f9dffe666e982451c9967c938d0c42730
Sinderion <jwinsatt@yahoo.com>
2018-05-22 15:04:09 -0400

    Basic Chat reports

--- ZLM_ChatReports.lua
+++ ZLM_ChatReports.lua
@@ -1,5 +1,3 @@
-
-
 local words = {
     -- add words to respond to
     "^lotto",
@@ -8,30 +6,68 @@ local words = {
     "^lottery"
 
 };
-local ZLMPrefixPattern = "%[Lotto\]";
-local ZLMPrefix = "[Lotto]";
+local ZLMPrefixPattern = "%[ZLM\]";
+local ZLMPrefix = "[ZLM]";
 local caseInsensitiveWords = {};
 for k,v in pairs(words) do
     -- replaces "words" with "[Ww][Oo][Rr][Dd][Ss]"
     caseInsensitiveWords[v],_ = string.gsub(v,"%a",function(a) return "["..a:upper()..a:lower().."]" end);
 end;
-function ZLM:ChatReport(player)
-    -- ***** Add prefix after testing
-    SendChatMessage(ZLMPrefix.."This is a test reply!", "WHISPER", nil, player);
-    SendChatMessage(ZLMPrefix.."More of a test reply!", "WHISPER", nil, player);
+function ZLM:PlayerName()
+    local player, _ = UnitName("player");
+    --Try to give back Sinderion-ShadowCouncil style name every time. No spaces, full name-realm.
+    --If the name is short, like Sinderion only, add realm.
+    if not string.match(player,"-") then
+        local realm = GetRealmName();
+        player = player.."-"..realm;
+        --player = player .."-".. string.gsub(realm,"%s", "");
+    end
+    --Remove Spaces
+    --player = string.gsub(player,"%s", "")
+    return player;
+end
+function ZLM:ChatReport(player,test)
+    local output = {};
+    local prefix;
+    local prefixpattern;
+    if test then
+     prefixpattern = "don'tmatchme";
+        prefix = "[test]"
+    else
+        prefixpattern = ZLMPrefixPattern;
+        prefix = ZLMPrefix;
+    end
+    SendChatMessage(prefix.." ZLM Standings:", "WHISPER", nil, player);
+    --Find if guy messaging is on the Scoreboard.
+    for i,v in ipairs(ZLM_ScoreboardData) do
+        if v.Name == ZLM:PlayerName() or v.Name == UnitName("player") then
+            -- Insert personalized report.
+            SendChatMessage(prefix.." Your rank: "..i, "WHISPER", nil, player);
+        end
+    end
+    --Reply with the whole list.
+    SendChatMessage(prefix.." Rank--Points--Name", "WHISPER", nil, player);
+    for i,v in ipairs(ZLM_ScoreboardData) do
+        local rank = i;
+        local character = v.Name;
+        local points = v.Points;
+        SendChatMessage(prefix..rank.." ....... "..points.." ....... "..character, "WHISPER", nil, player);
+    end
+    SendChatMessage(prefix.."------------------", "WHISPER", nil, player);
+    print("Lottery info sent to: "..player)
+
 end
 function ZLM:CHAT_MSG_WHISPER(event, message, author,...)
         for k, v in pairs(caseInsensitiveWords) do
             if not not string.match(message,v) then
-                ZLM:ChatReport(author);
+                local test = not not string.match(message,"^lottotest");
+                ZLM:ChatReport(author,test);
             end
         end
 end
 function ZLM_ChatFilter(self,event,myChatMessage, author,...)
-
     if type(myChatMessage) == "string" then
     --Hide whisper if it's our prefix
-    --print("it's a string!");
         if not not string.match(myChatMessage,ZLMPrefixPattern) then
             --print("It's a prefix!"..myChatMessage.." == "..ZLMPrefix);
             return true, myChatMessage, author, ...;
@@ -47,8 +83,8 @@ function ZLM_ChatFilter(self,event,myChatMessage, author,...)
     end
     return false, myChatMesssage, author,...
 end
-
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER",ZLM_ChatFilter);
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER_INFORM",ZLM_ChatFilter);
 ZLM:RegisterEvent("CHAT_MSG_WHISPER");
 
 
