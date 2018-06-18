cc3cbadbb68842bc5b02688b247aca9095a6f3bc
Charles Hunter <Charles082986@gmail.com>
2018-06-18 13:15:54 -0500

    Converted to ZLib

--- ZLM_Bountyboard.lua
+++ ZLM_Bountyboard.lua
@@ -1,137 +1,162 @@
-ZLM_Bountyboard = {};
-ZLM_Bountyboard.StructureArray = {
-    ItemId = { Type = ZLM_Table.Types.Input, Width = 0.05 },
-    Name = { Type = ZLM_Table.Types.InteractiveLabel, Width = 0.35 },
-    --Need = { Type = ZLM_Table.Types.Input, Width = 0.1 },
-    --OnHand = { Type = ZLM_Table.Types.Input, Width = 0.1 },
-    Points = { Type = ZLM_Table.Types.Input, Width = 0.1 },
-    HotItem = { Type = ZLM_Table.Types.Toggle, Width = 0.05 },
-    Delete = { Type = ZLM_Table.Types.Button, Width = 0.15 },
-};
-function ZLM_Bountyboard:new(title,callbacks,AceGUI)
-    if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
-    local topContainer = AceGUI:Create("Frame");
-    topContainer:SetLayout("Flow");
-    topContainer:SetTitle(title);
-    topContainer:SetCallback("OnClose",function(widget)
-        ZLM.bountyboard = nil;
-        widget:ReleaseChildren();
-        AceGUI:Release(widget);
-    end);
-    function topContainer:Terminate()
-        ZLM.bountyboard = nil;
-        self:ReleaseChildren();
-        self:Release();
-    end
-    topContainer.Table = ZLM_Table:new(self.StructureArray,{"ItemId"
-        ,"Name"
-        --,"Need"
-        --,"OnHand"
-        ,"Points"
-        ,"HotItem"
-        ,"Delete"},AceGUI);
-    function topContainer:AddRow(dataObj,AceGUI)
-        ZLM:Debug("Adding Row - " .. tostring(dataObj.ItemId));
-        -- dataObj structure: { ItemId = number, ItemLink = string, Points = number, HotItem = bool }
+ZLM_Bountyboard = {
+    Title = "ZLM - Bountyboard",
+    new = function(self,AceGUI,initialRows)
         if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
-        local rowObj = {};
-        rowObj.ItemId = {
-            Value = dataObj.ItemId
-            , OnEnterPressed = ZLM_Bountyboard_ItemIdChangeCallback
-        };
-        rowObj.Name = { Value = dataObj.Name, OnEnter = ZLM_BountyBoard_MakeTooltip, OnLeave = ZLM.ClearTooltip };
-        rowObj.Points = { Value = dataObj.Points,
-            OnEnterPressed = ZLM_Bountyboard_PointsChangedCallback
-        };
-        rowObj.HotItem = {
-            State = dataObj.HotItem,
-            OnValueChanged = ZLM_Bountyboard_HotItemChangeCallback
-        };
-        rowObj.Delete = {
-            Content = "Delete",
-            OnClick = ZLM_Bountyboard_DeleteCallback
-        };
-        self.Table.DataFrame:AddRow(rowObj,AceGUI);
-    end
-    local buttonContainer = AceGUI:Create("SimpleGroup");
-    buttonContainer:SetRelativeWidth(1);
-    buttonContainer:SetLayout("Flow");
-    buttonContainer:AddChild(ZLM_Button:new("Add Bounty",callbacks.AddBounty,0.25,AceGUI));
-    topContainer:AddChild(buttonContainer);
-    topContainer:AddChild(topContainer.Table.MainFrame);
-    for _,v in ipairs(ZLM.db.profile.Bounties) do
-        topContainer:AddRow(v,AceGUI)
-    end
-    return topContainer;
-end
-function ZLM_Bountyboard_ItemIdChangeCallback(me,_,text)
-    if not not text then
-        ZLM:Debug("ItemId Changed - " .. text);
-        local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(text);
-        ZLM:Wait(
-            0.25
-            ,ZLM_Bountyboard_WaitFunction_ItemIdChangeCallback
-            ,text
-            ,itemLink
-            ,me
-        );
-    end
-end
-function ZLM_Bountyboard_WaitFunction_ItemIdChangeCallback(text,itemLink,me)
-    ZLM:Debug("ItemId Changed - Wait Function - " .. text .. " - " .. itemLink);
-    local index = -1;
-    for i,v in ipairs(ZLM.db.profile.Bounties) do
-        if v.ItemId == text then index = i; break; end
-    end
-    if index < 1 then
-        index = #(ZLM.db.profile.Bounties) + 1;
-        ZLM.db.profile.Bounties[index] = {};
-    end
-    ZLM:Debug("ItemId Changed - Wait Function - " .. index);
-    ZLM.db.profile.Bounties[index].ItemId = text;
-    ZLM.db.profile.Bounties[index].Name = itemLink;
-    ZLM.db.profile.Bounties[index].Points = me.parent.children[3]:GetText();
-    ZLM.db.profile.Bounties[index].HotItem = me.parent.children[4]:GetValue();
-    ZLM:Debug("ItemId Changed - Wait Function - " .. ZLM.db.profile.Bounties[index].ItemLink);
-    me.parent.children[2]:SetText(itemLink);
-end
-function ZLM_Bountyboard_MakeTooltip(me,_,itemLink)
-    itemLink = itemLink or me.parent.children[2]:GetText();
-    ZLM:Debug("Making Bountyboard Tooltip - " .. tostring(itemLink));
-    if not not itemLink then
-        ZLM:MakeTooltip(itemLink);
-    end
-end
-function ZLM_Bountyboard_PointsChangeCallback(me,_,text)
-    local itemId = me.parent.children[1]:GetText();
-    if not not itemId and not not text then
-        text = tonumber(text);
-        for i,v in ipairs(ZLM.db.profile.Bounties) do
-            if v.ItemId == itemId then ZLM.db.profile.Bounties[i].Points = text; break; end
+        local topContainer = self:__CreateFrame(AceGUI);
+        topContainer.Table = ZLib.Table:new(AceGUI,1,self.StructureArray);
+        topContainer:AddChild(topContainer.Table);
+        topContainer.AddRow = self.__AddRow;
+        topContainer:AddChild(self:__CreateButtonContainer(AceGUI));
+        for _,v in ipairs(initialRows) do
+            topContainer:AddRow(AceGUI,v)
         end
-    end
-end
-function ZLM_Bountyboard_HotItemChangeCallback(me,_,value)
-    local itemId = me.parent.children[1].GetText();
-    if not not itemId then
-        for i,v in ipairs(ZLM.db.profile.Bounties) do
-            if v.itemId == itemId then ZLM.db.profile.Bounties[i].HotItem = value; break; end
+        return topContainer;
+    end,
+    __CreateFrame = function(self,AceGUI)
+        local frame = AceGUI:Create("Frame");
+        frame:SetLayout("Flow");
+        frame:SetTitle(self.Title);
+        return frame;
+    end,
+    __CreateButtonContainer = function(self,AceGUI)
+        local buttonContainer = AceGUI:Create("SimpleGroup");
+        buttonContainer:SetRelativeWidth(1);
+        buttonContainer:SetLayout("Flow");
+        buttonContainer:AddChild(ZLib.Button:new(AceGUI,1,"Add Bounty",self.__AddBounty));
+        return buttonContainer;
+    end,
+    __AddBounty = function() end,
+    __AddRow = function(self,AceGUI,oData)
+        oData = ZLM_Bountyboard:__ValidateData(oData);
+        ZLM:Debug("Adding Row - " .. tostring(oData.ItemId));
+        if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
+        local rowObj = {
+            ItemId = {
+                DefaultValue = oData.ItemId,
+                Width = ZLM_Bountyboard.StructureArray.ItemId.Width,
+                Callbacks = {
+                    OnEnterPressed = ZLM_Bountyboard.__ItemIdChangeCallback;
+                }
+            },
+            Name = {
+                Text = oData.Name,
+                Width = ZLM_Bountyboard.StructureArray.Name.Width,
+                Callbacks = {
+                    OnEnter = ZLM_Bountyboard.__MakeTooltip,
+                    OnLeave = ZLib.ClearTooltip
+                }
+            },
+            Points = {
+                DefaultValue = oData.Points,
+                Width = ZLM_Bountyboard.StructureArray.Points.Width,
+                Callbacks = {
+                    OnEnterPressed = ZLM_Bountyboard.__PointsChangeCallback;
+                }
+            },
+            HotItem = {
+                Width = ZLM_Bountyboard.StructureArray.HotItem.Width,
+                DefaultValue = oData.HotItem,
+                Callbacks = {
+                    OnValueChanged = ZLM_Bountyboard.__HotItemChangeCallback;
+                }
+            },
+            Delete = {
+                Text = "Delete",
+                Width = ZLM_Bountyboard.StructureArray.Delete.Width,
+                Callbacks = {
+                    OnClick = ZLM_Bountyboard.__DeleteClickCallback;
+                }
+            }
+        };
+        self.Table:AddRow(AceGUI,oData);
+    end,
+    __ValidateData = function(self,oData)
+        if not oData then oData = {}; end
+        if not ZLib:IsNumberValid(oData.ItemId) then oData.ItemId = 46002; end -- Worthless Piece of Violet Glass
+        local itemName, itemLink, _, _, _, _, _, _, _, _, _ = GetItemInfo(oData.ItemId);
+        if not ZLib:IsStringValid(oData.Name) then oData.Name = itemLink; end
+        if not ZLib:IsNumberValid(oData.Points) then oData.Points = 0; end;
+        if oData.HotItem == nil then oData.HotItem = false; end
+        return oData;
+    end,
+    __ItemIdChangeCallback = function(me,_,text)
+        if not not text then
+            ZLM:Debug("ItemId Changed - " .. text);
+            local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(text);
+            ZLM:Wait(
+                0.25
+                ,ZLM_Bountyboard.__Await.ItemIdChangeCallback
+                ,text
+                ,itemLink
+                ,me
+            );
         end
-    end
-end
-function ZLM_Bountyboard_DeleteCallback(me)
-    local itemId = me.parent.children[1].GetText();
-    if not not itemId then
-        for i,v in ipairs(ZLM.db.profile.Bounties) do
-            if v.itemId == itemId then
-                tremove(ZLM.db.profile.Bounties,i);
-                break;
+    end,
+    __Await = {
+        ItemIdChangeCallback = function(text,itemLink,me)
+            ZLM:Debug("ItemId Changed - Wait Function - " .. text .. " - " .. itemLink);
+            local index = -1;
+            for i,v in ipairs(ZLM.db.profile.Bounties) do
+                if v.ItemId == text then index = i; break; end
+            end
+            if index < 1 then
+                index = #(ZLM.db.profile.Bounties) + 1;
+                ZLM.db.profile.Bounties[index] = {};
+            end
+            ZLM:Debug("ItemId Changed - Wait Function - " .. index);
+            ZLM.db.profile.Bounties[index].ItemId = text;
+            ZLM.db.profile.Bounties[index].Name = itemLink;
+            ZLM.db.profile.Bounties[index].Points = me.parent.children[3]:GetText();
+            ZLM.db.profile.Bounties[index].HotItem = me.parent.children[4]:GetValue();
+            ZLM:Debug("ItemId Changed - Wait Function - " .. ZLM.db.profile.Bounties[index].ItemLink);
+            me.parent.children[2]:SetText(itemLink);
+        end
+    },
+    __MakeTooltip = function(me,_,itemLink)
+        itemLink = itemLink or me.parent.children[2]:GetText();
+        ZLM:Debug("Making Bountyboard Tooltip - " .. tostring(itemLink));
+        if not not itemLink then
+            ZLib:MakeTooltip(itemLink);
+        end
+    end,
+    __PointsChangeCallback = function(me,_,text)
+        local itemId = me.parent.children[1]:GetText();
+        if not not itemId and not not text then
+            text = tonumber(text);
+            for i,v in ipairs(ZLM.db.profile.Bounties) do
+                if v.ItemId == itemId then ZLM.db.profile.Bounties[i].Points = text; break; end
             end
         end
-        for i,v in ipairs(me.parent.parent.children) do
-            if v.children[1]:GetText() == itemId then
-                tremove(me.parent.parent.children,i);
+    end,
+    __HotItemChangeCallback = function(me_,value)
+        local itemId = me.parent.children[1].GetText();
+        if not not itemId then
+            for i,v in ipairs(ZLM.db.profile.Bounties) do
+                if v.itemId == itemId then ZLM.db.profile.Bounties[i].HotItem = value; break; end
             end
         end
-    end
-end
+    end,
+    __DeleteClickCallback = function(me)
+        local itemId = me.parent.children[1].GetText();
+        if not not itemId then
+            for i,v in ipairs(ZLM.db.profile.Bounties) do
+                if v.itemId == itemId then
+                    tremove(ZLM.db.profile.Bounties,i);
+                    break;
+                end
+            end
+            for i,v in ipairs(me.parent.parent.children) do
+                if v.children[1]:GetText() == itemId then
+                    tremove(me.parent.parent.children,i);
+                end
+            end
+        end
+    end,
+    StructureArray = {
+        ItemId = { Type = "EditBox", Width = 0.05 },
+        Name = { Type = "InteractiveLabel", Width = 0.35 },
+        Points = { Type = "EditBox", Width = 0.1 },
+        HotItem = { Type = "CheckBox", Width = 0.05 },
+        Delete = { Type = "Button", Width = 0.15 },
+    },
+};
--- ZLM_Scoreboard.lua
+++ ZLM_Scoreboard.lua
@@ -1,54 +1,102 @@
-ZLM_Scoreboard = {};
-function ZLM_Scoreboard:new(title,callbacks,defaultValues,AceGUI)
-    if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
-    --BEGIN: Creating the primary Scoreboard frame.
-    local topContainer = AceGUI:Create("Frame");
-    topContainer:SetLayout("Flow");
-    topContainer:SetTitle(title);
-    topContainer:SetCallback("OnClose",function(widget)
-        ZLM.scoreboard = nil;
-        widget:ReleaseChildren();
-        AceGUI:Release(widget);
-    end);
-    function topContainer:Terminate()
-        ZLM.scoreboard = nil;
-        self:ReleaseChildren();
-        self:Release();
-    end
-    --END: Creating the primary Scoreboard frame.
-    --BEGIN: Creating the button container.
-    local buttonContainer = AceGUI:Create("SimpleGroup");
-    buttonContainer:SetLayout("Flow");
-    buttonContainer:SetRelativeWidth(0.35);
-    --END: Creating the button container.
-    --BEGIN: Creating the datepicker container.
-    local datepickerContainer = AceGUI:Create("SimpleGroup");
-    datepickerContainer:SetLayout("Flow");
-    datepickerContainer:SetRelativeWidth(0.65);
-    --END: Creating the datepicker container.
-    --BEGIN: Creating Datepickers
-    local startDateDatePicker = ZLM_DatePicker:new("Start Date","ScoreboardStartDateTime",2018,date("%Y"),true,callbacks.DateChanged,defaultValues.StartDate,AceGUI);
-    local endDateDatePicker = ZLM_DatePicker:new("End Date","ScoreboardEndDateTime",2018,date("%Y"),true,callbacks.DateChanged,defaultValues.EndDate,AceGUI);
-    --END: Creating Datepickers
-    --BEGIN: Creating Table
-    topContainer.Table = ZLM_Table:new({
-        Rank = { Width = 0.1, Type = ZLM_Table.Types.Label },
-        Name = { Width = 0.35, Type = ZLM_Table.Types.Label },
-        Points = { Width = 0.25, Type = ZLM_Table.Types.Label },
-        Min = { Width = 0.15, Type = ZLM_Table.Types.Label },
-        Max = { Width = 0.15, Type = ZLM_Table.Types.Label },
-    },{"Rank","Name","Points","Min","Max"}, AceGUI);
-    function topContainer:AddRow(dataObj,AceGUI)
+ZLM_Scoreboard = {
+    Title = "ZLM - Scoreboard",
+    StructureArray = {
+        Rank = { Width = 0.1, Type = "Label" },
+        Name = { Width = 0.35, Type = "Label" },
+        Points = { Width = 0.25, Type = "Label" },
+        Min = { Width = 0.15, Type = "Label" },
+        Max = { Width = 0.15, Type = "Label" },
+    };
+    new = function(self,AceGUI)
         if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
-        local rowObj = {};
-        for k,v in pairs(dataObj) do
-            rowObj[k] = { Content = v };
-        end
-        self.Table.DataFrame:AddRow(rowObj,AceGUI);
-    end
-    topContainer.StartDatePicker = startDateDatePicker;
-    topContainer.EndDatePicker = endDateDatePicker;
-    function topContainer:SetDatePickersToPreviousMonth()
+        local frame = self:__CreateFrame(AceGUI);
+        frame.ButtonContainer = self:__CreateButtonContainer(AceGUI);
+        frame:AddChild(frame.ButtonContainer);
+        frame.Table =  self:__CreateTable(AceGUI);
+        frame:AddChild(frame.Table);
+        frame.AddRow = self:__AddRow;
+        return frame;
+    end,
+    __CreateFrame = function(self,AceGUI)
+        if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
+        local frame = AceGUI:Create("Frame");
+        frame:SetLayout("Flow");
+        frame:SetTitle(self.Title);
+        return frame;
+    end,
+    __CreateButtonContainer = function(self,AceGUI)
+        if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
+        local container = AceGUI:Create("SimpleGroup");
+        container:SetLayout("Flow");
+        container:SetRelativeWidth(0.35);
+        container:AddChild(self.__CreatePreviousWeekButton(AceGUI));
+        container:AddChild(self.__CreateLastFourWeeksButton(AceGUI));
+        container:AddChild(self.__CreatePreviousMonthButton(AceGUI));
+        container:AddChild(self.__CreateSinceLastDrawingButton(AceGUI));
+        return container;
+    end,
+    __CreatePreviousMonthButton = function(self,AceGUI)
+        return ZLib.Button:new(AceGUI,0.5,"Previous Month",{ OnClick = ZLM_Scoreboard.__SetDatePickersToPreviousMonth });
+    end,
+    __CreateLastFourWeeksButton = function(self,AceGUI)
+        return ZLib.Button:new(AceGUI,0.5,"Last 28 Days",{ OnClick = ZLM_Scoreboard.__SetDatePickersToLastFourWeeks });
+    end,
+    __CreatePreviousWeekButton = function(self,AceGUI)
+        return ZLib.Button:new(AceGUI,0.5,"Previous Week",{ OnClick = ZLM_Scoreboard.__SetDatePickersToPreviousWeek });
+    end,
+    __CreateSinceLastDrawingButton = function(self,AceGUI)
+        return ZLib.Button:new(AceGUI,0.5,"Since Last Drawing",{ OnClick = ZLM_Scoreboard.__SetDatePickersToPreviousWeek });
+    end,
+    __CreateDatePickerContainer = function(self,AceGUI)
+        if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
+        local container = AceGUI:Create("SimpleGroup");
+        container:SetLayout("Flow");
+        container:RelativeWidth(0.65);
+        container.StartDateTimePicker = ZLib.DateTimePicker:new(AceGUI,1,{ Multiline = true, DefaultValue = ZLM.db.global.Reporting.Scoreboard.StartDateTime },{ OnValueChanged = self.__StartDateTimeChangedCallback });
+        container.EndDateTimePicker = ZLib.DateTimePicker:new(AceGUI,1,{ Multiline = true, DefaultValue = ZLM.db.global.Reporting.Scoreboard.EndDateTime },{ OnValueChanged = self.__EndDateTimeChangedCallback });
+        container:AddChild(container.StartDateTimePicker);
+        container:AddChild(container.EndDateTimePicker);
+        return container;
+    end,
+    __CreateTable = function(self,AceGUI)
+        if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
+        return ZLib.Table:new(AceGUI,1,self.StructureArray)
+    end,
+    __StartDateTimeChangedCallback = function(picker,dtObject,key,value,error)
+        if ZLib:IsStringValid(error) then ZLM:Debug(error,4); return false; end
+        ZLM.db.Global.Reporting.Scoreboard.StartDateTime = dtObject;
+    end,
+    __EndDateTimeChangedCallback = function(picker,dtObject,key,value,error)
+        if ZLib:IsStringValid(error) then ZLM:Debug(error,4); return false; end
+        ZLM.db.Global.Reporting.Scoreboard.EndDateTime = dtObject;
+    end,
+    __AddRow = function(self,AceGUI,oData)
+        if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
+        local rowObj = {
+            Rank = {
+                Text = oData.Rank,
+                Width = ZLM_Scoreboard.StructureArray.Rank.Width;
+            },
+            Name = {
+                Text = oData.Name,
+                Width = ZLM_Scoreboard.StructureArray.Name.Width;
+            },
+            Points = {
+                Text = oData.Points,
+                Width = ZLM_Scoreboard.StructureArray.Points.Width;
+            },
+            Min = {
+                Text = oData.Min,
+                Width = ZLM_Scoreboard.StructureArray.Min.Width;
+            },
+            Max = {
+                Text = oData.Max,
+                Width = ZLM_Scoreboard.StructureArray.Max.Width;
+            }
+        };
+        self.Table:AddRow(AceGUI,rowObj);
+    end,
+    __SetDatePickersToPreviousMonth = function(self)
         local daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         local d = date("*t");
         d.month = d.month -1;
@@ -57,37 +105,37 @@ function ZLM_Scoreboard:new(title,callbacks,defaultValues,AceGUI)
         d.hour = 0;
         d.minute = 0;
         d.sec = 0;
-        self.StartDatePicker:SetDate(d);
+        self.StartDatePicker:SetValue(d);
         d.day = daysInMonth[d.month];
         d.hour = 23;
         d.minute = 59;
         d.sec = 59;
-        self.EndDatePicker:SetDate(d)
-    end
-    function topContainer:SetDatePickersToLastFourWeeks()
-        self.StartDatePicker:SetDatePast({ day = 28, DayState = 0});
-        self.EndDatePicker:SetDatePast({ day = 1, DayState = 1});
-    end
-    function topContainer:SetDatePickersToPreviousWeek()
+        self.EndDatePicker:SetValue(d)
+    end,
+    __SetDatePickersToLastFourWeeks = function(self)
+        self.StartDateTimePicker:SetDatePast({ day = 28, DayState = 0});
+        self.EndDateTimePicker:SetDatePast({ day = 1, DayState = 1});
+    end,
+    __SetDatePickersToPreviousWeek = function(self)
         local d = date("%w");
         d = d - 2;
         if d < 0 then d = d + 7; end
-        self.StartDatePicker:SetDatePast({ day = d + 7, DayState = 0; });
-        self.EndDatePicker:SetDatePast({ day = d + 1, DayState = 1});
-    end
-    function topContainer:SetDatePickersToLastDrawing()
+        self.StartDateTimePicker:SetDatePast({ day = d + 7, DayState = 0; });
+        self.EndDateTimePicker:SetDatePast({ day = d + 1, DayState = 1});
+    end,
+    __SetDatePickersToLastDrawing = function(self)
         local dateObj = ZLM.db.global.LastDrawing;
         if dateObj == nil then dateObj = { year = 2018, month = 1, day = 1, hour = 0, minute = 0, sec = 0}; end
-        self.StartDatePicker:SetDate(dateObj);
-        self.EndDatePicker:SetDate(date("*t"));
+        self.StartDateTimePicker:SetValue(dateObj);
+        self.EndDateTimePicker:SetValue(date("*t"));
     end
+};
+function ZLM_Scoreboard:new(title,callbacks,defaultValues,AceGUI)
+    if not AceGUI then AceGUI = LibStub("AceGUI-3.0"); end
     --END: Creating Table
     datepickerContainer:AddChild(startDateDatePicker);
     datepickerContainer:AddChild(endDateDatePicker);
-    datepickerContainer:AddChild(ZLM_Button:new("Previous Month",function() topContainer:SetDatePickersToPreviousMonth(); end,1/2,AceGUI));
-    datepickerContainer:AddChild(ZLM_Button:new("Last 28 Days",function() topContainer:SetDatePickersToLastFourWeeks(); end,1/2,AceGUI));
-    datepickerContainer:AddChild(ZLM_Button:new("Previous Week",function() topContainer:SetDatePickersToPreviousWeek(); end,1/2,AceGUI));
-    datepickerContainer:AddChild(ZLM_Button:new("Since Last Drawing",function() topContainer:SetDatePickersToLastDrawing(); end,1/2,AceGUI));
+
     buttonContainer:AddChild(ZLM_Heading:new("Actions",AceGUI));
     buttonContainer:AddChild(ZLM_Button:new("Calculate Scoreboard",callbacks.UpdateScoreboard,1,AceGUI));
     buttonContainer:AddChild(ZLM_Button:new("Get Winners",callbacks.GetWinners,1,AceGUI));
